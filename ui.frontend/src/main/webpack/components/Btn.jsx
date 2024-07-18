d955cbce0e8a62591aa09d4ae7f21e51b57134cb
Sahil <Sahil.jakhar@auspost.com.au>
2024-07-18 16:14:11 +1000

    Style System using Parent classes updated

--- ui.frontend/src/main/webpack/components/Btn.jsx
+++ ui.frontend/src/main/webpack/components/Btn.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { Button } from "@auspost/postmaster-react";
 
 const Btn = (props) => {
-  console.log(props);
+  const regex = /variant-/i;
+  const getVariant = props.classes.split(" ").filter((wrd) => regex.test(wrd));
+  const variantUsed =
+    getVariant.length > 0 ? getVariant[0].replace(regex, "") : "primary";
   return (
     <>
-      <Button variant="primary" size="medium">
+      <Button variant={variantUsed} size="medium">
         {props.text} anything
       </Button>
     </>
